2cbae09ce62c915fad62ea08751244e70228d4da
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-17 17:43:03 -0700

    wrote character counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-  
+$(function(){
+  $('.new-tweet textarea').on('keyup', function(){
+    var availableChars = 140 - $(this).val().length;
+    var counterCount = $(this).parent().find('.counter').html(availableChars);
+    if(availableChars < 0) {
+      counterCount.css({'color': 'red'});      
+    } else {
+      counterCount.css({'color': ''});
+    }
+  });
 });
