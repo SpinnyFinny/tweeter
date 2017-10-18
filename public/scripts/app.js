424b54cf0f89d8ab21bd3a15d54ef6e595257f05
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-17 21:42:39 -0700

    created/implemented createTweetElement function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,44 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(function(){
 
+  var tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+
+  function createTweetElement(tweetData){
+    
+    var tweet = $(`<article class="tweet card-container">
+    <header>
+        <img class='avatar' src=${tweetData.user.avatars.regular}>
+        <h2>${tweetData.user.name}</h2>
+        <span class="tweet-author">${tweetData.user.handle}</span>
+    </header>
+      <div class="tweet-body clearfix">
+        <p>${tweetData.content.text}</p>
+      </div>
+    <footer class="clearfix">
+      <span>${tweetData.created_at} ago</span>
+      <div class="mini-icons">
+        <img src="/images/flag.png">
+        <img src="/images/repost.png">
+        <img src="/images/heart.png">
+      </div>
+    </footer>
+    </article>`)
+    return tweet;
+  }
+  
+  var $tweet = createTweetElement(tweetData);
+  $('#tweet-container').append($tweet); 
+});
