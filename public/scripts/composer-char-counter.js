6d080b68c8d3bab5025239abd47cde38f6a20ed8
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-18 20:42:20 -0700

    refactored

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(function(){
   $('.tweet-composer textarea').on('keyup', function(){
     var availableChars = 140 - $(this).val().length;
-    var counterCount = $(this).parent().find('.counter').html(availableChars);
+    var counterCount = $('#char-counter').html(availableChars);
     if(availableChars < 0) {
       counterCount.css({'color': 'red'});      
     } else {
