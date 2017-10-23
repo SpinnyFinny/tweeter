6b4e11ffdef2cbbf479ebe5768b1bc19b93ad259
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-22 21:22:16 -0700

    formatted and linted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,57 @@
-$(function() {
-
-  var tweetTemplate = $("#displayed-tweet-template").html();
+$(function () {
+  var tweetTemplate = $('#displayed-tweet-template').html();
   var compiledTweetTemplate = Handlebars.compile(tweetTemplate);
 
-  Handlebars.registerHelper('convertTime', function(timeStamp){
+  Handlebars.registerHelper('convertTime', function (timeStamp) {
     return moment(timeStamp).fromNow();
-  })
+  });
 
-  function loadTweets() {
+  function loadTweets () {
     $.ajax({
-      url: "/tweets",
-      method: "GET",
+      url: '/tweets',
+      method: 'GET',
       dataType: 'json'
-    }).done(function(data) {
-      $("#tweets-container").html(compiledTweetTemplate(data));
-    })
+    }).done(function (data) {
+      $('#tweets-container').html(compiledTweetTemplate(data));
+    });
   }
 
-  var newTweetForm = $(".tweet-composer form");
-  newTweetForm.on('submit', function(event){
+  var newTweetForm = $('.tweet-composer form');
+  newTweetForm.on('submit', function (event) {
     event.preventDefault();
-    var tweetText = $('#tweet-text').val()
-    $('#tweet-text').on('focus', function() {
-      $('#submit-tweet-error').html('').css({'visibility': 'hidden'})
+    var tweetText = $('#tweet-text').val();
+    $('#tweet-text').on('focus', function () {
+      $('#submit-tweet-error')
+        .html('')
+        .css({ visibility: 'hidden' });
     });
-    if(tweetText.length <= 0){
-      $('#submit-tweet-error').html('Please enter some text').css({'visibility': 'visible'})
+    if (tweetText.length <= 0) {
+      $('#submit-tweet-error')
+        .html('Please enter some text')
+        .css({ visibility: 'visible' });
       return;
     } else if (tweetText.length > 140) {
-      $('#submit-tweet-error').html('Your tweet is too long').css({'visibility': 'visible'})
+      $('#submit-tweet-error')
+        .html('Your tweet is too long')
+        .css({ visibility: 'visible' });
       return;
     }
     $.ajax({
-      url: "/tweets",
-      method: "POST",
+      url: '/tweets',
+      method: 'POST',
       data: $(this).serialize(),
       context: this
-    }).done(function() {
+    }).done(function () {
       this.reset();
       $('#char-counter').html(140);
       loadTweets();
     });
-  })
-  
-  $('#compose-button').on('click', function() {
+  });
+
+  $('#compose-button').on('click', function () {
     $('.tweet-composer').slideToggle();
     $('#tweet-text').focus();
   });
-  
+
   loadTweets();
 });
